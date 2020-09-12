c9e88a7f89d4130cea5029b287972d273b3a6ec0
Sedherthe <siddhartha.soma1@gmail.com>
2020-09-12 14:25:35 +0530

    Setup Firebase for the project

---
+++ src/config/fb.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCmFX0lhIZPBR3DM4E85S4bKjqjyLx-qh4",
+  authDomain: "marioplan-e9ab1.firebaseapp.com",
+  databaseURL: "https://marioplan-e9ab1.firebaseio.com",
+  projectId: "marioplan-e9ab1",
+  storageBucket: "marioplan-e9ab1.appspot.com",
+  messagingSenderId: "124824066890",
+  appId: "1:124824066890:web:61bc1d140ba2dbea937fb3",
+  measurementId: "G-JED5KSB3WW"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+firebase.firestore().settingss({ timestampsInSnapshots: true });
+
+export default firebase;
